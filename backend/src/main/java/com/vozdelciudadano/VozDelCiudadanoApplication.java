3d51f84642f07e6ccb4b83121300529f01719efd
PaulLeon10 <paul.leon.g@uni.pe>
2026-06-05 14:49:46 -0400

    Commit final, la aplicacion ya esta lista, se agrega la clase principal

---
+++ backend/src/main/java/com/vozdelciudadano/VozDelCiudadanoApplication.java
@@ -0,0 +1,12 @@
+package com.vozdelciudadano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozDelCiudadanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VozDelCiudadanoApplication.class, args);
+    }
+}
